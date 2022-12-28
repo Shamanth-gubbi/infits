532a61f5cebbd6c38c76b0794de00bd673065764
Avinash Sahu <sahuavinash412@gmail.com>
2022-12-28 20:19:26 +0530

    canged .html to .php

--- Avinash/event_calendar.php
+++ Avinash/event_calendar.php
@@ -320,45 +320,7 @@
         <span style="font-size:20px;cursor:pointer; margin: 5px;" onclick="openNav()">&#9776; <span>Menu</span></span>
     </div>
 
-    <div id="content">
-        <div class="content-container">
-            <div class="user-img-container">
-                <div class="profile"></div>
-                <h3>John Wayne</h3>
-            </div>
-            <div class="flex-center-container">
-                <div class="cards-container">
-                    <div class="cards" id="card1">
-                        <img src="assets/icons/card-icon1.svg" alt="My Profile">
-                        <p>My Profile</p>
-                    </div>
-                    <div class="cards" id="card2">
-                        <img src="assets/icons/card-icon1.svg" alt="Referral Code">
-                        <p>Referral Code</p>
-                    </div>
-                    <div class="cards" id="card3">
-                        <img src="assets/icons/card-icon2.svg" alt="About Us">
-                        <p>About Us</p>
-                    </div>
-                    <div class="cards" id="card4">
-                        <img src="assets/icons/card-icon3.svg" alt="My Profile">
-                        <p>My Profile</p>
-                    </div>
-                    <div class="cards" id="card5">
-                        <img src="assets/icons/card-icon1.svg" alt="Referral Code">
-                        <p>Referral Code</p>
-                    </div>
-                    <div class="cards" id="card6">
-                        <img src="assets/icons/card-icon4.svg" alt="About Us">
-                        <p>About Us</p>
-                    </div>
-                </div>
-            </div>
-            <div class="logout-btn-box">
-                <button class="logout">Logout</button>
-            </div>
-        </div>
-    </div>
+
 
 
     <script>
