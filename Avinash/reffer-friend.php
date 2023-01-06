7121f2d34e4d57322b404a0467e1a52c100a18bb
Avinash Sahu <sahuavinash412@gmail.com>
2023-01-06 20:50:23 +0530

    Added reffer-friend.php

---
+++ Avinash/reffer-friend.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refer Code</title>
+    <link rel="stylesheet" href="assets/styles/reffer.css">
+</head>
+<body>
+
+<?php include 'event_calendar.php'; ?>
+
+    <div id="content">
+        <h1>Refer to a friend</h1>
+        <div class="flex-container">
+            <div class="refer-code-container">
+                <img src="assets/images/istockphoto-1394169876-612x612 1.svg" alt="SVG">
+                <div class="refer-code-box">
+                    <p id="refer-code">mir20220320 <button id="copyBtn">Copy</button></p>
+                    <button id="share">Share</button>
+                </div>
+            </div>
+            <div class="friend-list-box">
+                <div class="heading-box">
+                    <h3>Invite a friend</h3>
+                    <img src="assets/icons/search.svg" alt="Search">
+                </div>
+                <ul>
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span>Invite</span>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span>Invite</span>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span class="accepted">Accepted</span>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span>Invite</span>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span>Invite</span>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="user">
+                            <img src="assets/images/friend1.svg" alt="">
+                            <div class="para-box">
+                                <p class="name">Tongkun Lee</p>
+                                <p class="company">Facebook</p>
+                            </div>
+                        </div>
+                        <div class="status">
+                            <span>Invite</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
