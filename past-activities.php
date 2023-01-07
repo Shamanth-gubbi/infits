b7c61393c19a046125a549eb5b651faedd691c3e
Avinash Sahu <sahuavinash412@gmail.com>
2023-01-07 20:17:26 +0530

    added Activities.php

---
+++ past-activities.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infits | Activities</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <style>
+        #content {
+            overflow: auto;
+            height: 90%;
+            font-family: 'Poppins';
+            font-style: normal;
+            padding: 10px;
+            margin: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="page">
+        <?php include 'event_calendar.php'; ?>
+
+        <div id="content">
+            <div class="first-box">
+                <div class="title-box">
+                    <h1>Past Activities</h1>
+                    <div class="filter-box">
+                        <button>Custom Dates <img src="assets/icons/Vector (1).svg"></button>
+                        <button>Year</button>
+                        <button>Month</button>
+                        <button class="active">Week</button>
+                    </div>
+                </div>
+                <img src="assets/images/Group 1000004735.svg" alt="Steps">
+            </div>
+            <div class="activity-containers">
+                <h4>19 Jun 2021</h4>
+                <div class="activities-box">
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="activity-containers">
+                <h4>19 Jun 2021</h4>
+                <div class="activities-box">
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="activity-containers">
+                <h4>19 Jun 2021</h4>
+                <div class="activities-box">
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                    <div class="activities">
+                        <div class="details">
+                            <img src="assets/icons/Vector.svg" alt="">
+                            <div class="para-box">
+                                <p class="activity-name">Running</p>
+                                <p class="time">11:10 a.m</p>
+                            </div>
+                        </div>
+                        <div class="distance">
+                            <img src="assets/icons/Frame.svg">
+                            <p class="value">9070 m</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+</body>
+
+</html>
