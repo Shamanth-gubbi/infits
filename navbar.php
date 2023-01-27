e9cb88b138b0ddea217e0d8d542be681c3aefcfd
Avinash Sahu <sahuavinash412@gmail.com>
2023-01-28 01:51:37 +0530

    Created blank form && form details page

--- navbar.php
+++ navbar.php
@@ -7,11 +7,11 @@
     <title>Sidebar menu With Sub-menus | Using HTML, CSS & JQuery</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2"> -->
 </head>
 
 <style>
-    @import url('https://fonts.googleapis.com/earlyaccess/nats.css');
+    /* @import url('https://fonts.googleapis.com/earlyaccess/nats.css'); */
     @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
 
