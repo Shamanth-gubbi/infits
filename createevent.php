62df7d0af054008c528591e687fe3b928a5d69e0
Janhavikom <84494006+Janhavikom@users.noreply.github.com>
2023-01-29 16:40:26 +0530

    Update createevent.php

--- createevent.php
+++ createevent.php
@@ -82,9 +82,232 @@ include("navbar.php");
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Create Event</title>
-    <link rel="stylesheet" href="createevent.css" />
+    <!-- <link rel="stylesheet" href="createevent.css" /> -->
 </head>
 <style>
+html {
+    overflow-x: hidden;
+}
+
+#content {
+    display: flex;
+    flex-direction: column;
+    height: 90%;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    /* font-size: 35px; */
+    /* padding: 20px; */
+}
+
+.event-form {
+    /* width: 50%; */
+    /* padding: 2%; */
+    margin-left: 15%;
+    margin-right: 15%;
+    /* background-color: palegoldenrod; */
+}
+
+.ev-img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* width: 260px;
+height: 190px; */
+}
+
+.event-image {
+    width: 120px;
+    height: 120px;
+}
+
+.event_title {
+    font-size: 12px;
+    font-weight: 800;
+    /* margin-bottom: 15px; */
+}
+
+.eve_form {
+    /* background-color: azure; */
+    margin:0 auto;
+    padding: 10px;
+    font-size:12px !important;
+    width: 70%;
+
+}
+
+.subject {
+    display: block;
+    /* font-size: 18px;
+    font-weight: bold; */
+    width: 100%;
+}
+
+.rem-item {
+    background: #FFFFFF;
+    border: 1px solid #4B9AFB;
+    border-radius: 45px;
+    padding: 5px;
+    margin-top: 10px;
+    font-size: 12px;
+}
+
+.rem_icon {
+    color: #4B9AFB;
+    margin-right: 15px;
+}
+
+
+
+.content-name {
+    position: absolute;
+    bottom: 5px;
+    left: 0px;
+    transition: all 0.3s ease;
+}
+
+
+
+.form_btn {
+    width: 30%;
+    border-color: #4B9AFB;
+    border-radius: 10px;
+    color: white;
+    text-align: center;
+    padding: 10px;
+    margin-top: 30px;
+}
+
+.txt {
+    width: 100%;
+    /* margin-bottom: 10px; */
+    /* margin-top: 15px; */
+    cursor: pointer;
+}
+
+.txt p {
+    padding-left: 15px;
+}
+.txt a {
+    padding-left: 15px;
+}
+
+.txticon {
+    padding: 10px;
+}
+
+.input-icons i {
+    position: absolute;
+}
+
+.input-icons {
+    width: 100%;
+    margin-bottom: 10px;
+    border: none;
+}
+
+.icon {
+    padding: 10px;
+    color: black;
+    min-width: 50px;
+}
+
+.input-field {
+    width: 100%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 50px;
+}
+
+/* POPUP */
+
+.pop-box .button {
+    font-size: 15px;
+    text-decoration: none;
+    color: black;
+    border: 1px solid black;
+    border-radius: 20px;
+    margin-top: 20px;
+    padding: 10px 20px;
+}
+
+.bg-popContainer {
+    display: none;
+}
+
+.pop-box {
+    width: 100%;
+    height: auto;
+    background: #FFFFFF;
+    border: 1px solid rgba(225, 225, 225, 0.66);
+    box-shadow: 0px 11px 25px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    position: relative;
+}
+
+.cont {
+    border-radius: 20px;
+}
+
+input {
+    width: 50%;
+    display: block;
+    margin: 15px auto;
+    padding: 5px;
+}
+
+.closer {
+    position: absolute;
+    top: 0;
+    right: 14px;
+    font-size: 30px;
+    transform: rotate(45deg);
+    cursor: pointer;
+}
+
+::placeholder {
+    color: black;
+    opacity: 1;
+    /* Firefox */
+}
+
+:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: black;
+}
+
+::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: black;
+}
+
+/* MEDIA QUERY */
+
+
+@media screen and (min-width: 720px) {
+
+    /* Styles go here */
+    .mobile-menu {
+        display: none;
+    }
+}
+
+@media (min-width: 0px) and (max-width: 720px) {
+
+
+    .event-form {
+        /* height: 100%; */
+        margin-left: 0 !important;
+        width: 100%;
+        display: flex;
+        /* justify-content: center; */
+        justify-content: space-around;
+        align-items: center;
+        flex-direction: column;
+    }
+
+}
+
 /* Appointment popup */
 .modal {
     display: none !important;
@@ -148,14 +371,16 @@ include("navbar.php");
             <div class="ev-img">
                 <img class="event-image" src="images/event_image.svg" alt="">
             </div>
-            <br>
-
+            
 
             <form action="#" method="post">
                 <div class="eve_form">
                     <!-- Event name field -->
-                    <label for="subject" class="event_title">EVENT NAME</label>
-                    <select class="subject" type="text" name="subject" placeholder="Category" style="padding:10px 0px;"
+                    <label for="subject" class="event_title" style="margin-bttom:5px">EVENT NAME</label>
+                    <select class="subject" type="text" name="subject" placeholder="Category" style="padding:10px 0px;background: #FFFFFF;
+border: 1px solid #F1F1F1;
+box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+border-radius: 8px;"
                         required>
                         <option value="Consultation">Consultation</option>
                         <option value="Dietplan">Diet Plan</option>
@@ -190,7 +415,7 @@ include("navbar.php");
                         <div class="input-icons">
                             <i class="fa-solid fa-user icon">
                             </i>
-                            <select style="border-top:none;border-left:none;border-right:none" name="clientname"
+                            <select style="border-bottom:1.8px solid #EFEFEF;border-top:none;border-left:none;border-right:none" name="clientname"
                                 class="input-field" required>
                                 <option value="" disabled selected>Add Client</option>
                                 <?php
@@ -209,7 +434,7 @@ include("navbar.php");
                         <div class="input-icons">
                             <i class="fa-solid fa-suitcase icon">
                             </i>
-                            <select style="border-top:none;border-left:none;border-right:none" name="meetingtype"
+                            <select style="border-bottom:1.8px solid #EFEFEF;border-top:none;border-left:none;border-right:none" name="meetingtype"
                                 class="input-field" required>
                                 <option value="" disabled selected>Meeting Type</option>
                                 <option value="Videocall">Video Call</option>
@@ -220,7 +445,7 @@ include("navbar.php");
 
 
                         <!-- Date and Time fields -->
-                        <div class="txt button" style="border-bottom:1.8px solid black;" id="button">
+                        <div class="txt button" style="border-bottom:1.8px solid #EFEFEF" id="button">
                             <i class="fa-solid fa-calendar-days txticon" style="display:inline-block">
                             </i>
                             <p style="display:inline-block">Date and Time</p>
@@ -248,7 +473,7 @@ include("navbar.php");
                         <div class="input-icons">
                             <i class="fa-solid fa-location icon">
                             </i>
-                            <input style="border-top:none;border-left:none;border-right:none" class="input-field"
+                            <input style="border-bottom:1.8px solid #EFEFEF;border-top:none;border-left:none;border-right:none" class="input-field"
                                 type="text" placeholder="Place of meeting" name="placeofmeeting">
                         </div>
 
@@ -256,7 +481,7 @@ include("navbar.php");
                         <div class="input-icons">
                             <i class="fa-solid fa-bars icon">
                             </i>
-                            <input style="border-top:none;border-left:none;border-right:none" class="input-field"
+                            <input style="border-bottom:1.8px solid #EFEFEF;border-top:none;border-left:none;border-right:none" class="input-field"
                                 type="text" placeholder="Add Description" name="description" required>
                         </div>
 
@@ -264,12 +489,12 @@ include("navbar.php");
                         <div class="input-icons">
                             <i class="fa-solid fa-paperclip icon">
                             </i>
-                            <input style="border-top:none;border-left:none;border-right:none" class="input-field"
+                            <input style="border-bottom:1.8px solid #EFEFEF;border-top:none;border-left:none;border-right:none" class="input-field"
                                 type="text" placeholder="Attachment" name="attachment" required>
                         </div>
                     </div>
 
-                    <div style="width:100%; margin-left:10%; margin-right:10%">
+                    <div style="width:100%; margin-left:20%; margin-right:10%">
                         <a href="createevent.php"><input style="display:inline-block; color:black; background:white;"
                                 class="form_btn" placeholder="Cancel"></input></a>
                         <button style="display:inline-block; background: #4B9AFB;" class="form_btn" name="submit"
